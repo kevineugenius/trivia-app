ec87c7f2d5d8f04d7ea081831b511391edbbf5ea
Kevin Smith <kevineugenius@gmail.com>
2022-09-03 14:14:54 -0400

    keeping this for posterity, contains basic router and a couple functional components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom"
 import "./App.css";
+
+import Landing from "./components/Landing/Landing";
+import Quiz from "./components/Quiz/Quiz";
+import Results from "./components/Results/Results";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
-      <body>
-        Empty app
-      </body>
+      <Routes>
+        <Route path="/" element={ <Landing /> } />
+        <Route path="/quiz" element={ <Quiz /> } />
+        <Route path="/results" element={ <Results /> } />
+      </Routes>
     </div>
   );
 }
